bed4420b003b418184a8b1acfe2ea3c10dbd6650
jwills801 <jwills@zagmail.gonzaga.edu>
2020-11-01 10:43:31 -0600

    fixed initial condition for analytical solution

--- Advection_Diffusion.m
+++ Advection_Diffusion.m
@@ -2,10 +2,10 @@
 % 1D Advection diffusion by using an explicit forward in time, centered in
 % space scheme.
 clear
-N = 21;
-tfinal = .5;
+N = 201;
+tfinal = 5;
 L = 2;
-dt = .05;
+dt = .0005;
 nstep = tfinal/dt;
 U=1;
 D=.05;
@@ -24,7 +24,6 @@ time = 0.0;
 for j=1:N
     f(j) = .5*sin(2*pi*k*x(j));
 end
-f_exact = f;
 
 % Time Loop
 h = animatedline('color','r');
@@ -33,7 +32,7 @@ axis([0 L -1.5 1.5]);
 legend('Numerical Solution','Exact Solution')
 for m = 1:nstep
     % Exact Solution
-    f_exact = exp(-D*k^2*time)*sin(2*pi*k*(x-U*time));
+    f_exact = .5*exp(-4*pi^2*D*k^2*time)*sin(2*pi*k*(x-U*time));
     
     
     % Store solution
@@ -49,12 +48,13 @@ for m = 1:nstep
     f(1) = f(N);
     time = time + dt;
     % plot
+    clearpoints(h)
+    clearpoints(h2)
     addpoints(h,x,f)
     addpoints(h2,x,f_exact)
     drawnow limitrate
     pause(dt*3)
-    clearpoints(h)
-    clearpoints(h2)
+    
     
 end
     
